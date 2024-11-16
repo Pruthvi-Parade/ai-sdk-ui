bed39818c1dd5bcb732b67beae26ee4c16a7fa44
Pruthvi-Parade <parade.pruthvi@gmail.com>
2024-11-16 13:02:48 +0530

    removed sender messages in langchain

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,29 +1,24 @@
-import { StreamingTextResponse, LangChainStream } from 'ai'
-import { ChatAnthropic } from 'langchain/chat_models/anthropic'
-import { AIMessage, HumanMessage } from 'langchain/schema'
+import { StreamingTextResponse } from 'ai'
+import { Anthropic } from '@anthropic-ai/sdk'
 
 export const runtime = 'edge'
 
 export async function POST(req) {
   const { messages } = await req.json()
-  const { stream, handlers } = LangChainStream()
 
-  const llm = new ChatAnthropic({
-    anthropicApiKey: process.env.ANTHROPIC_API_KEY,
-    streaming: true,
+  const anthropic = new Anthropic({
+    apiKey: process.env.ANTHROPIC_API_KEY,
   })
 
-  llm
-    .call(
-      messages.map((m) =>
-        m.role === 'user'
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content)
-      ),
-      {},
-      [handlers]
-    )
-    .catch(console.error)
+  const stream = await anthropic.messages.create({
+    messages: messages.map(m => ({
+      role: m.role === 'user' ? 'user' : 'assistant',
+      content: m.content
+    })),
+    model: 'claude-3-opus-20240229',
+    stream: true,
+    max_tokens: 1024,
+  })
 
   return new StreamingTextResponse(stream)
 }
