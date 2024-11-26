b852d9c86911c0c698ae10d6b64cbc2be2cf4fe7
Pruthvi-Parade <parade.pruthvi@gmail.com>
2024-11-26 22:28:10 +0530

    Added the Document in the database

--- app/firebase/config.ts
+++ app/firebase/config.ts
@@ -1,10 +1,12 @@
 import { initializeApp, FirebaseApp, getApps, getApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 
 interface FirebaseConfig {
   apiKey: string | undefined;
   authDomain: string | undefined;
+  databaseURL: string | undefined;
   projectId: string | undefined;
   storageBucket: string | undefined;
   messagingSenderId: string | undefined;
@@ -14,6 +16,7 @@ interface FirebaseConfig {
 const firebaseConfig: FirebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
@@ -22,5 +25,6 @@ const firebaseConfig: FirebaseConfig = {
 
 // Initialize Firebase
 const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const database = getDatabase(app); 
 const auth = getAuth(app);
-export {app, auth};
+export {app, auth, database};
