edf546e54fec40be14714cbaaad47a4da66a6ef9
Pruthvi-Parade <parade.pruthvi@gmail.com>
2024-12-02 18:28:10 +0530

    Attempt to solve bug for private id

--- app/firebase/server-config.ts
+++ app/firebase/server-config.ts
@@ -6,18 +6,18 @@ import { getDatabase } from "firebase-admin/database";
 const formattedPrivateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
 const serviceAccount = {
-  type: process.env.NEXT_PUBLIC_TYPE,
-  project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
-  private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
-  private_key: formattedPrivateKey,
-  client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
-  client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
-  auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
-  token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
+  type: process.env.NEXT_PUBLIC_TYPE || "",
+  project_id: process.env.NEXT_PUBLIC_PROJECT_ID || "",
+  private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID || "",
+  private_key: formattedPrivateKey || "",
+  client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL || "",
+  client_id: process.env.NEXT_PUBLIC_CLIENT_ID || "",
+  auth_uri: process.env.NEXT_PUBLIC_AUTH_URI || "",
+  token_uri: process.env.NEXT_PUBLIC_TOKEN_URI || "",
   auth_provider_x509_cert_url:
-    process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.NEXT_PUBLIC_UNIVERSAL_DOMAIN,
+    process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL || "",
+  client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL || "",
+  universe_domain: process.env.NEXT_PUBLIC_UNIVERSAL_DOMAIN || "",
 };
 
 const firebaseAdminConfig = {
