cacadc09b1b72b98d876535ed61392f4b5e334c5
Pruthvi-Parade <parade.pruthvi@gmail.com>
2024-12-02 18:26:18 +0530

    Attempt to solve bug for private key

--- app/firebase/server-config.ts
+++ app/firebase/server-config.ts
@@ -1,11 +1,15 @@
 import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getDatabase } from "firebase-admin/database";
+
+// Ensure that the private key is properly formatted
+const formattedPrivateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/g, '\n');
+
 const serviceAccount = {
   type: process.env.NEXT_PUBLIC_TYPE,
   project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
   private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
-  private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+  private_key: formattedPrivateKey,
   client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
   client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
   auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
